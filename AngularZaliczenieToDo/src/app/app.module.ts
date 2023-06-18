fea2b76fb1212b02ca8ef0f1b553ba8ea83dbf2e
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2023-06-18 23:05:39 +0200

    form end

--- AngularZaliczenieToDo/src/app/app.module.ts
+++ AngularZaliczenieToDo/src/app/app.module.ts
@@ -13,6 +13,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
